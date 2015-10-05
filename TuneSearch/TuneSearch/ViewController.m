d49df46d81e9f9f70a97d5d6a3b5fd2c93cc6a0b
Mike Henry <chsmike@gmail.com>
2015-10-05 13:42:17 -0400

    Reachability wired up

--- TuneSearch/TuneSearch/ViewController.m
+++ TuneSearch/TuneSearch/ViewController.m
@@ -7,7 +7,6 @@
 //
 
 #import "ViewController.h"
-#import "Reachability.h"
 
 @interface ViewController ()
 
@@ -23,21 +22,81 @@ Reachability *wifiReach;
 bool internetAvailable;
 bool serverAvailable;
 
+#pragma mark - Interactivity Methods
+
+- (IBAction)getDataPressed:(id)sender {
+    NSLog(@"Get data");
+}
+
 #pragma mark - Network Methods
 
+- (void)updateReachabilityStatus:(Reachability *)currReach {
+    NSParameterAssert([currReach isKindOfClass:[Reachability class]]);
+    NetworkStatus netStatus = [currReach currentReachabilityStatus];
+    if (currReach == hostReach) {
+        switch (netStatus) {
+            case NotReachable:
+                NSLog(@"Server not reachable");
+                serverAvailable = false;
+                break;
+            case ReachableViaWiFi:
+                NSLog(@"Server reachable via Wifi");
+                serverAvailable = true;
+                break;
+            case ReachableViaWWAN:
+                NSLog(@"Server reachable via WAN");
+                serverAvailable = true;
+                break;
+            default:
+                break;
+        }
+    }
+    if (currReach == internetReach) {
+        switch (netStatus) {
+            case NotReachable:
+                NSLog(@"Internet not reachable");
+                internetAvailable = false;
+                break;
+            case ReachableViaWiFi:
+                NSLog(@"Internet reachable via Wifi");
+                internetAvailable = true;
+                break;
+            case ReachableViaWWAN:
+                NSLog(@"Internet reachable via WAN");
+                internetAvailable = true;
+                break;
+            default:
+                break;
+        }
+    }
+}
 
+- (void)reachabilityChanged:(NSNotification *)note {
+    Reachability *currReach = [note object];
+    [self updateReachabilityStatus:currReach];
+}
 
 
 #pragma mark - Life Cycle Methods
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    _hostName = @"itunes.apple.com/lookup?id=909253";
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(reachabilityChanged:) name:kReachabilityChangedNotification object:nil];
+    
+    hostReach = [Reachability reachabilityWithHostName:_hostName];
+    [hostReach startNotifier];
+    [self updateReachabilityStatus:hostReach];
+    
+    internetReach = [Reachability reachabilityWithHostName:_hostName];
+    [internetReach startNotifier];
+    [self updateReachabilityStatus:internetReach];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    
 }
 
 @end
